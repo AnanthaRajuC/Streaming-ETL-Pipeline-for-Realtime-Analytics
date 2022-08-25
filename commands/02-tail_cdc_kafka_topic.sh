91e774d6fcce2ed87d87fa4cbd21cd7d6891f778
Anantha Raju C <anantha@platformcommons.com>
2022-08-25 08:49:33 +0530

    UPDATES

--- commands/02-tail_cdc_kafka_topic.sh
+++ commands/02-tail_cdc_kafka_topic.sh
@@ -57,7 +57,38 @@ curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" 
 
 
 
-
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" 127.0.0.1:8083/connectors/ -d '{
+  "name": "jdbc-sink",
+  "config": {
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "database.hostname": "172.17.0.1",
+    "database.port": "3306",
+    "database.user": "debezium",
+    "database.password": "Debezium@123#",
+    "database.server.name": "mysql",
+	  "database.server.id": "223344",
+    "database.include.list": "streaming_etl_db",
+	"topics" : "person_address_enriched",
+	"auto.create" : "true",
+        "auto.evolve" : "true",
+        "insert.mode" : "upsert",
+		"pk.fields" : "P_ID",
+	  "database.allowPublicKeyRetrieval": true,
+	  "database.history.kafka.bootstrap.servers": "kafka:9092",
+	  "database.history.kafka.topic": "mysql-streaming_etl_db-person",
+	  "time.precision.mode": "connect",
+    "include.schema.changes": false,
+    "transforms": "unwrap,dropTopicPrefix",
+	  "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+	  "transforms.dropTopicPrefix.type":"org.apache.kafka.connect.transforms.RegexRouter",
+	  "transforms.dropTopicPrefix.regex":"asgard.demo.(.*)",
+	  "transforms.dropTopicPrefix.replacement":"$1",
+	  "key.converter":"org.apache.kafka.connect.json.JsonConverter",
+	  "key.converter.schemas.enable": "false",
+	  "value.converter":"org.apache.kafka.connect.json.JsonConverter",
+	  "value.converter.schemas.enable": "false"
+  }
+}'
 
 
 
